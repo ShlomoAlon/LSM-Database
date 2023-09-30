3a1cf354f3b04ecb4fe4a0db59284ae36c9eae14
shlomoalon <shlomoalon9@gmail.com>
2023-09-30 19:48:39 -0400

    d

--- src/memtable.rs
+++ src/memtable.rs
@@ -1,25 +1,33 @@
+use arrayvec::ArrayVec;
+use futures::io::AsyncWriteExt;
+use glommio::io::{DmaStreamWriter, ImmutableFile, ImmutableFileBuilder, ReadResult};
+use itertools::Itertools;
+use positioned_io::RandomAccessFile;
+use positioned_io::ReadAt;
+use rayon::prelude::*;
 use std::cmp::Ordering::{Equal, Greater, Less};
 use std::fmt::format;
 use std::fs::File;
+use std::io::BufWriter;
 use std::io::Write;
 use std::path::Path;
-use positioned_io::ReadAt;
-use positioned_io::RandomAccessFile;
-use rayon::prelude::*;
-use itertools::Itertools;
-use std::io::BufWriter;
-use arrayvec::ArrayVec;
+use futures::executor::block_on;
+// use futures_lite::io::AsyncWriteExt;
+use glommio::{
+    io::{DmaFile, DmaStreamWriterBuilder},
+    LocalExecutor,
+};
 
-const PAGE_SIZE: usize = 1024;
+const PAGE_SIZE: usize = 1024 * 4;
 const VECTOR_SIZE: usize = PAGE_SIZE / 16;
 type Child = Option<Box<Node>>;
 
-struct Node{
+struct Node {
     key: i64,
     value: i64,
     height: i64,
     left: Child,
-    right: Child
+    right: Child,
 }
 
 // struct Node_Iterator{
@@ -35,15 +43,14 @@ struct Node{
 //     }
 // }
 
-impl Node{
-
-    fn new(key: i64, value: i64) -> Box<Self>{
-        Box::new(Node{
+impl Node {
+    fn new(key: i64, value: i64) -> Box<Self> {
+        Box::new(Node {
             key: key,
             value: value,
             height: 1,
             left: None,
-            right: None
+            right: None,
         })
     }
 
@@ -56,17 +63,12 @@ impl Node{
     }
 
     fn fix_height(&mut self) {
-        self.height = 1 + std::cmp::max(
-            self.left_height(),
-            self.right_height()
-        );
+        self.height = 1 + std::cmp::max(self.left_height(), self.right_height());
     }
 
     fn insert(node: Child, key: i64, value: i64) -> Box<Self> {
         match node {
-            None => {
-                Node::new(key, value)
-            }
+            None => Node::new(key, value),
             Some(mut inner_node) => {
                 if key < inner_node.key {
                     inner_node.left = Some(Node::insert(inner_node.left, key, value));
@@ -82,12 +84,16 @@ impl Node{
         node.fix_height();
         let balance = node.right_height() - node.left_height();
         if balance > 1 {
-            if node.right.as_ref().unwrap().right_height() < node.right.as_ref().unwrap().left_height() {
+            if node.right.as_ref().unwrap().right_height()
+                < node.right.as_ref().unwrap().left_height()
+            {
                 node.right = Some(Node::rotate_right(node.right.unwrap()));
             }
             node = Node::rotate_left(node);
         } else if balance < -1 {
-            if node.left.as_ref().unwrap().left_height() < node.left.as_ref().unwrap().right_height() {
+            if node.left.as_ref().unwrap().left_height()
+                < node.left.as_ref().unwrap().right_height()
+            {
                 node.left = Some(Node::rotate_left(node.left.unwrap()));
             }
             node = Node::rotate_right(node);
@@ -95,7 +101,7 @@ impl Node{
         node
     }
 
-    fn rotate_left(mut node: Box<Node>) -> Box<Node>{
+    fn rotate_left(mut node: Box<Node>) -> Box<Node> {
         let mut top = node.right.take().unwrap();
         node.right = top.left.take();
         node.fix_height();
@@ -103,7 +109,7 @@ impl Node{
         top.fix_height();
         top
     }
-    fn rotate_right(mut node: Box<Node>) -> Box<Node>{
+    fn rotate_right(mut node: Box<Node>) -> Box<Node> {
         let mut top = node.left.take().unwrap();
         node.left = top.right.take();
         node.fix_height();
@@ -126,11 +132,11 @@ impl Node{
         }
     }
 
-    fn get(&self, key: i64) -> Option<i64>{
+    fn get(&self, key: i64) -> Option<i64> {
         match self.key.cmp(&key) {
             Equal => Some(self.value),
             Greater => self.left.as_ref().and_then(|node| node.get(key)),
-            Less => self.right.as_ref().and_then(|node| node.get(key))
+            Less => self.right.as_ref().and_then(|node| node.get(key)),
         }
     }
 
@@ -153,34 +159,48 @@ impl Node{
             }
         }
     }
-    fn write_all(&self, mut buffer: & mut BufWriter<File>){
+    fn get_all(&self, vec: &mut Vec<u8>) {
+        if let Some(left) = &self.left {
+            left.get_all(vec);
+        }
+        vec.extend_from_slice(&mut self.key.to_ne_bytes());
+        vec.extend_from_slice(&mut self.value.to_ne_bytes());
+        if let Some(right) = &self.right {
+            right.get_all(vec);
+        }
+    }
+    fn write_all(&self, mut buffer: &mut BufWriter<File>) {
         if let Some(left) = &self.left {
             left.write_all(buffer);
         }
-        buffer.write_all(&self.key.to_ne_bytes()).expect("did not write");
-        buffer.write_all(&self.value.to_ne_bytes()).expect("did not write");
+        buffer
+            .write_all(&self.key.to_ne_bytes())
+            .expect("did not write");
+        buffer
+            .write_all(&self.value.to_ne_bytes())
+            .expect("did not write");
         if let Some(right) = &self.right {
             right.write_all(buffer);
         }
     }
 }
 
-struct MemoryTable{
+struct MemoryTable {
     mem_table_size: usize,
     cur_size: usize,
-    root: Child
+    root: Child,
 }
 
-impl MemoryTable{
+impl MemoryTable {
     fn new(mem_table_size: usize) -> Self {
-        MemoryTable{
+        MemoryTable {
             mem_table_size: mem_table_size,
             cur_size: 0,
-            root: None
+            root: None,
         }
     }
 
-    fn insert(&mut self, key: i64, value: i64) -> bool{
+    fn insert(&mut self, key: i64, value: i64) -> bool {
         if self.cur_size >= self.mem_table_size {
             return false;
         }
@@ -189,20 +209,23 @@ impl MemoryTable{
         true
     }
 
-    fn to_vec_2(&self) -> Vec<i64>{
+    fn to_vec_2(&self) -> Vec<i64> {
         let tuples = Node::scan(&self.root, i64::MIN, i64::MAX);
-        tuples.iter().flat_map(|(key, value)| vec![*key, *value]).collect()
+        tuples
+            .iter()
+            .flat_map(|(key, value)| vec![*key, *value])
+            .collect()
     }
 
-    fn scan(&self, key1: i64, key2: i64) -> Vec<(i64, i64)>{
+    fn scan(&self, key1: i64, key2: i64) -> Vec<(i64, i64)> {
         Node::scan(&self.root, key1, key2)
     }
 
-    fn get(&self, key: i64) -> Option<i64>{
+    fn get(&self, key: i64) -> Option<i64> {
         self.root.as_ref().and_then(|node| node.get(key))
     }
 
-    fn create_stable(&self, file_name: String) -> SSTable{
+    fn create_stable(&self, file_name: String) -> SSTable {
         // println!("started writing");
 
         // let bytes = self.to_vec();
@@ -213,35 +236,43 @@ impl MemoryTable{
         //     result
         // }).collect::<Vec<_>>();
         let file_size = self.cur_size * 16;
-        let mut file = std::fs::File::create(&file_name).unwrap();
-        let mut file = BufWriter::new(file);
+        // let mut file = std::fs::File::create(&file_name).unwrap();
+        // let mut file = BufWriter::new(file);
         match &self.root {
             None => {}
             Some(i) => {
-                i.write_all(&mut file);
+                let mut bytes = Vec::with_capacity(file_size * 16);
+                i.get_all(&mut bytes);
+                let executor = LocalExecutor::default();
+                executor.run(async {
+                    let mut file = DmaFile::create(&file_name).await.unwrap();
+                    let mut writer = DmaStreamWriterBuilder::new(file).build();
+                    writer.write_all(&bytes).await.unwrap();
+                    writer.sync().await.unwrap();
+                    writer.close().await.unwrap();
+                });
             }
         }
         // for (key, value) in items {
         //     file.write_all(&key.to_ne_bytes()).expect("did not write");
         //     file.write_all(&value.to_ne_bytes()).expect("did not write");
         // }
-        file.flush().expect("did not flush");
         // println!("ended writing");
-        SSTable{
+        SSTable {
             file_name: file_name,
-            file_size: file_size
+            file_size: file_size,
         }
     }
 }
 
 #[derive(Debug)]
-struct SSTable{
+struct SSTable {
     file_name: String,
     file_size: usize,
 }
 
-impl SSTable{
-    fn get_all(&self) -> Vec<(i64, i64)>{
+impl SSTable {
+    fn get_all(&self) -> Vec<(i64, i64)> {
         let file = RandomAccessFile::open(&self.file_name).unwrap();
         let pages_to_search: Vec<_> = (0..(self.file_size + (PAGE_SIZE - 1)) / PAGE_SIZE).collect();
         let mut result = Vec::new();
@@ -251,13 +282,34 @@ impl SSTable{
         }
         result
     }
-    fn get_page(&self, key: u64, file: &RandomAccessFile) -> ArrayVec<(i64, i64), VECTOR_SIZE>{
+    fn get_page_2(&self, key: u64, file: &DmaFile) -> ArrayVec<(i64, i64), VECTOR_SIZE> {
+        let executor = LocalExecutor::default();
+        let mut vec = ArrayVec::new();
+        let s = file.read_at_aligned(key * PAGE_SIZE as u64, PAGE_SIZE);
+        let s = executor.run(async{
+
+            println!("reading");
+            let file = file
+                .read_at(key * PAGE_SIZE as u64, PAGE_SIZE)
+                .await
+                .unwrap();
+            println!("read");
+            file
+        });
+        for i in (0..s.len()).step_by(16) {
+            let key = i64::from_ne_bytes(s[i..i + 8].try_into().unwrap());
+            let value = i64::from_ne_bytes(s[i + 8..i + 16].try_into().unwrap());
+            vec.push((key, value));
+        }
+        vec
+    }
+    fn get_page(&self, key: u64, file: &RandomAccessFile) -> ArrayVec<(i64, i64), VECTOR_SIZE> {
         let mut bytes: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
         let bytes_read = file.read_at(key * PAGE_SIZE as u64, &mut bytes).unwrap();
         let mut vec = ArrayVec::new();
         for i in (0..bytes_read).step_by(16) {
-            let key = i64::from_ne_bytes(bytes[i..i+8].try_into().unwrap());
-            let value = i64::from_ne_bytes(bytes[i+8..i+16].try_into().unwrap());
+            let key = i64::from_ne_bytes(bytes[i..i + 8].try_into().unwrap());
+            let value = i64::from_ne_bytes(bytes[i + 8..i + 16].try_into().unwrap());
             vec.push((key, value));
         }
         vec
@@ -267,12 +319,14 @@ impl SSTable{
         let pages_to_search: Vec<_> = (0..(self.file_size + (PAGE_SIZE - 1)) / PAGE_SIZE).collect();
         let mut result = None;
         _ = pages_to_search.binary_search_by(|&page_num| {
+            println!("getting page");
             let page = self.get_page(page_num as u64, &file);
+            println!("got page");
             let first_key = page.first().unwrap().0;
             let last_key = page.last().unwrap().0;
 
             if key > last_key {
-                return Less
+                return Less;
             };
             if first_key > key {
                 Greater
@@ -289,18 +343,23 @@ impl SSTable{
         });
         result
     }
-    fn scan(&self, key1: i64, key2: i64) -> Vec<(i64, i64)>{
-        let file = RandomAccessFile::open(&self.file_name).unwrap();
+    fn scan(&self, key1: i64, key2: i64) -> Vec<(i64, i64)> {
+        let executor = LocalExecutor::default();
+        let file = executor.run(async {
+            DmaFile::open(&self.file_name).await.unwrap()
+        });
         let pages_to_search: Vec<_> = (0..(self.file_size + (PAGE_SIZE - 1)) / PAGE_SIZE).collect();
         let mut first_page = ArrayVec::new();
         let mut cur_page_index = pages_to_search.len();
         _ = pages_to_search.binary_search_by(|&page_num| {
-            let page = self.get_page(page_num as u64,&file);
+            println!("getting page");
+            let page = self.get_page_2(page_num as u64, &file);
+            println!("got page");
             let first_key = page.first().unwrap().0;
             let last_key = page.last().unwrap().0;
 
             if key1 > last_key {
-                return Less
+                return Less;
             };
             first_page = page;
             cur_page_index = page_num;
@@ -316,7 +375,10 @@ impl SSTable{
             return vec![];
         }
         let first_page_starting_element_index = first_page.partition_point(|&(key, _)| key < key1);
-        let mut most_recent_page: ArrayVec<(i64, i64), VECTOR_SIZE> = first_page[first_page_starting_element_index..].try_into().unwrap();
+        let mut most_recent_page: ArrayVec<(i64, i64), VECTOR_SIZE> = first_page
+            [first_page_starting_element_index..]
+            .try_into()
+            .unwrap();
         let mut result = Vec::new();
         loop {
             // if most_recent_page.last().is_none() {
@@ -324,35 +386,30 @@ impl SSTable{
             //     dbg!(self.clone());
             // }
             if most_recent_page.last().unwrap().0 > key2 {
-                let page_partition_point = most_recent_page.partition_point(|&(key, _)| key <= key2);
+                let page_partition_point =
+                    most_recent_page.partition_point(|&(key, _)| key <= key2);
                 result.extend(most_recent_page[..page_partition_point].to_vec());
                 break;
             }
             result.extend(most_recent_page);
             cur_page_index += 1;
-            if cur_page_index == pages_to_search.len()  {
+            if cur_page_index == pages_to_search.len() {
                 break;
             }
-            most_recent_page = self.get_page(pages_to_search[cur_page_index] as u64, &file);
+            most_recent_page = self.get_page_2(pages_to_search[cur_page_index] as u64, &file);
         }
         result
     }
 }
 
-
-
-
-
-
-pub struct Database{
+pub struct Database {
     table_size: u64,
     path: String,
     mem_table: MemoryTable,
-    ss_tables: Vec<SSTable>
+    ss_tables: Vec<SSTable>,
 }
 
 impl Database {
-
     pub fn new(path: String, table_size: u64) -> Self {
         let path = Path::new(&path);
         if path.exists() {
@@ -360,7 +417,6 @@ impl Database {
         }
         std::fs::create_dir(path).unwrap();
         Self::from_disk(path.to_str().unwrap().to_string(), table_size)
-
     }
     pub fn from_disk(path: String, table_size: u64) -> Self {
         let items = std::fs::read_dir(path.clone()).unwrap();
@@ -369,83 +425,103 @@ impl Database {
             let item = item.unwrap();
             let file_name = item.file_name().into_string().unwrap();
             if file_name.ends_with(".sst") {
-                ss_table_names.push(SSTable{
+                ss_table_names.push(SSTable {
                     file_name: file_name,
-                    file_size: item.metadata().unwrap().len() as usize
+                    file_size: item.metadata().unwrap().len() as usize,
                 });
             }
         }
         ss_table_names.sort_by(|a, b| {
-            let a_file_number = a.file_name.split(".").next().unwrap().parse::<u64>().unwrap();
-            let b_file_number = b.file_name.split(".").next().unwrap().parse::<u64>().unwrap();
+            let a_file_number = a
+                .file_name
+                .split(".")
+                .next()
+                .unwrap()
+                .parse::<u64>()
+                .unwrap();
+            let b_file_number = b
+                .file_name
+                .split(".")
+                .next()
+                .unwrap()
+                .parse::<u64>()
+                .unwrap();
             a_file_number.cmp(&b_file_number)
-        }
-        );
-        Database{
+        });
+        Database {
             table_size,
             path: path.clone(),
             mem_table: MemoryTable::new(10000),
-            ss_tables: ss_table_names.into_iter().map(|name| {
-                SSTable{
+            ss_tables: ss_table_names
+                .into_iter()
+                .map(|name| SSTable {
                     file_name: format!("{}/{}", path.clone(), name.file_name),
-                    file_size: name.file_size
-                }
-            }).collect()
+                    file_size: name.file_size,
+                })
+                .collect(),
         }
     }
     pub fn insert(&mut self, key: i64, value: i64) {
-        if self.mem_table.insert(key, value){
+        if self.mem_table.insert(key, value) {
         } else {
-            let new_stable = self.mem_table.create_stable(format!("{}/{}.sst", self.path, self.ss_tables.len()));
+            let new_stable =
+                self.mem_table
+                    .create_stable(format!("{}/{}.sst", self.path, self.ss_tables.len()));
             self.ss_tables.push(new_stable);
             self.mem_table = MemoryTable::new(self.table_size as usize);
             self.mem_table.insert(key, value);
         }
     }
 
-    pub fn scan(&self, key1: i64, key2: i64) -> Vec<(i64, i64)>{
-        let mut scanned_tables = self.ss_tables.par_iter().map(
-            |ss_table| {
-                ss_table.scan(key1, key2)
-            }).collect::<Vec<_>>();
+    pub fn scan(&self, key1: i64, key2: i64) -> Vec<(i64, i64)> {
+        let mut scanned_tables = self
+            .ss_tables
+            .iter()
+            .map(|ss_table| ss_table.scan(key1, key2))
+            .collect::<Vec<_>>();
         let value = Node::scan(&self.mem_table.root, key1, key2);
         scanned_tables.push(value);
-        scanned_tables.into_iter().kmerge_by(|a, b| a.0 < b.0).collect()
+        scanned_tables
+            .into_iter()
+            .kmerge_by(|a, b| a.0 < b.0)
+            .collect()
     }
 
-    pub fn get(&self, key: i64) -> Option<i64>{
+    pub fn get(&self, key: i64) -> Option<i64> {
         let mut result = self.mem_table.get(key);
         if result.is_some() {
             return result;
         }
-        self.ss_tables.par_iter().find_map_any(
-            |ss_table| {
-                ss_table.get_value(key)
-            }
-        )
+        self.ss_tables
+            .par_iter()
+            .find_map_any(|ss_table| ss_table.get_value(key))
     }
-    pub fn close(&mut self){
-        self.ss_tables.push(self.mem_table.create_stable(format!("{}/{}.sst", self.path, self.ss_tables.len())));
+    pub fn close(&mut self) {
+        self.ss_tables.push(self.mem_table.create_stable(format!(
+            "{}/{}.sst",
+            self.path,
+            self.ss_tables.len()
+        )));
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
     use super::*;
+    use std::path::Path;
 
     #[test]
     fn test_insert() {
         let mut node = Node::new(1, 1);
-        for i in -10000..10000{
+        for i in -10000..10000 {
             node = Node::insert(Some(node), i, i);
         }
         // dbg!(Node::scan(&Some(node), -10, 10000));
         let mut writer = BufWriter::new(File::create("test.txt").unwrap());
         node.write_all(&mut writer);
-        let ss_table = SSTable{
+        let ss_table = SSTable {
             file_name: "test.txt".to_string(),
-            file_size: 10000 * 16 * 2
+            file_size: 10000 * 16 * 2,
         };
         // dbg!(ss_table.get_all());
         dbg!(ss_table.scan(-10, 10000));
@@ -463,7 +539,7 @@ mod tests {
     }
 
     #[test]
-    fn test_database(){
+    fn test_database() {
         let mut db = Database::new("Database".to_string(), 10000);
         for i in (0..10000000).map(|x| x * 2) {
             db.insert(i, i);
@@ -479,4 +555,3 @@ mod tests {
         db.close();
     }
 }
-
